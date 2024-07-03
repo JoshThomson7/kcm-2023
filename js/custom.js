da70054f3f2c90990603ef08cb60f7c998b15a89
JoshThomson7 <josh@fl1.digital>
2024-07-03 12:38:41 +0100

    Styles

--- js/custom.js
+++ js/custom.js
@@ -23,9 +23,11 @@
 // @codekit-prepend "../lib/isotope/_isotope.pkgd.min.js";
 // @codekit-prepend "../lib/flatpickr/_flatpickr.js";
 
+
 // Includes
 // @codekit-prepend "./inc/_helpers.js";
 // @codekit-prepend "./inc/_widget-filterify.js";
+
 
 // Modules
 // @codekit-prepend "../modules/advanced-video-banners/js/_avb.js";
@@ -35,7 +37,7 @@ jQuery(function($) {
 
     $().loadDependencies();
     $().tooltips();
-    //$().stickyMenu();
+    $().stickyMenu();
     $().mobileMenu('nav#nav_mobile', 'left');
     $().smoothScroll();
     $().chosenSelect();
@@ -56,25 +58,4 @@ jQuery(function($) {
             autoplay: 1,
         },
     });
-
-    function updateCartTotal() {
-        $.ajax({
-            url: ajaxurl,
-            type: 'GET',
-            dataType: 'json',
-            data: { action: 'update_cart_total_with_vat' },
-            success: function(response) {
-                $('.cart-total-vat .total-amount').html(response.data.cart_total);
-            }
-        });
-    }
-
-    // Update the cart total on document load
-    updateCartTotal();
-
-    // Listen for changes to the cart through WooCommerce events
-    $(document).on('added_to_cart removed_from_cart', function() {
-        updateCartTotal();
-    });
-
 });
